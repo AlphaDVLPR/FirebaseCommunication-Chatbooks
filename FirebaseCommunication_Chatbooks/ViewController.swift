273ae66a76c759dd3c629825070615809386b63b
AlphaDVLPR <thephoenixbarber@gmail.com>
2021-06-15 11:12:38 -0600

    Initial Commit

---
+++ FirebaseCommunication_Chatbooks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirebaseCommunication_Chatbooks
+//
+//  Created by Jesse Rae on 6/15/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
